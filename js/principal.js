57183705af113c56fb0685c6aa78d7eee07e0c96
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-10 22:42:41 -0300

    Calculando o IMC e atualizando a tabela atraves do querySelector()

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,17 @@
-var titulo = document.querySelector(".titulo");
 
-titulo.textContent = "Aparecida Nutricionista"
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var peso = paciente.querySelector(".info-peso").textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+
+var imc = peso / (altura * altura);
+
+paciente.querySelector(".info-imc").textContent = imc
+
+console.log(paciente);
+console.log(altura);
+console.log(peso);
+console.log(imc);
