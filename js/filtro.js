c68aea93de78f6633c1a5d066b4197535cedef4d
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-17 03:21:17 -0300

    *** Parei em 08:27 da aula 08 item 1 ***

---
+++ js/filtro.js
@@ -0,0 +1,7 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+
+    *** Parei em 08:27 da aula 08 item 1 ***
+})
